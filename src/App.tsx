a28a88812ec8902957cea4bdb3ebcc3f13951a6a
PaulSamPs <psamoylenko2@gmail.com>
2022-06-27 09:09:11 +0300

    начал новую верстка страницы друзей

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,6 @@ import { Messages } from './pages/Messages/Messages';
 import { Dialogs } from './pages/Dialogs/Dialogs';
 import { Conversations } from './pages/Conversations/Conversations';
 import { UserInfo } from './pages/UserInfo/UserInfo';
-import { Friends } from './pages/Friends/Friends';
-import { Requests } from './pages/Requests/Requests';
 import { FriendsList } from './pages/FriendsList/FriendsList';
 import { People } from './pages/People/People';
 
@@ -43,10 +41,7 @@ export const App = () => {
             <Route path='conversations' element={<Conversations />} />
             <Route path='people' element={<People />} />
             <Route path='people/:id' element={<UserInfo />} />
-            <Route path='friends' element={<FriendsList />}>
-              <Route index element={<Friends />} />
-              <Route path='requests' element={<Requests />} />
-            </Route>
+            <Route path='friends' element={<FriendsList />} />
           </Route>
           <Route path='*' element={<Navigate to='/' replace />} />
         </Routes>
--- src/pages/FriendsList/FriendsList.tsx
+++ src/pages/FriendsList/FriendsList.tsx
@@ -1,25 +1,56 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 import styles from './Friendslist.module.scss';
-import { CustomLink } from '../../components/CustomLink/CustomLink';
-import { Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useRequest } from '../../hooks/useRequest';
+import { API_URL } from '../../http/axios';
+import { Button } from '../../components/Button/Button';
+import { ReactComponent as SearchIcon } from '../../helpers/icons/search.svg';
 
 export const FriendsList = () => {
-  const { request } = useRequest();
+  const { friends } = useRequest();
+  const [text, setText] = React.useState<string | null>('');
+  console.log(text);
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.nav}>
-        <CustomLink to='' className={styles.name}>
-          Друзья
-        </CustomLink>
-        <CustomLink to='requests' className={styles.name}>
-          Заявки в друзья {request.length > 0 && <div>{request.length}</div>}
-        </CustomLink>
+    <>
+      <div className={styles.borderTop} />
+      <div className={styles.wrapper}>
+        <form className={styles.search}>
+          <div
+            className={styles.input}
+            id='input'
+            contentEditable='true'
+            placeholder='Поиск друзей...'
+            role='textbox'
+            aria-multiline='true'
+            onInput={(e: FormEvent<HTMLDivElement>) => setText(e.currentTarget.textContent)}
+          ></div>
+          <Button appearance='primary' disabled={true}>
+            <SearchIcon />
+          </Button>
+        </form>
+        <div className={styles.grid}>
+          {friends.length > 0 ? (
+            friends.map((f) => (
+              <div className={styles.card} key={f.id}>
+                <Link to={`/main/people/${f.id}`} replace className={styles.followers}>
+                  <img
+                    src={f.avatar == null ? `/photo.png` : `${API_URL}/avatar/${f.avatar}`}
+                    alt={f.name}
+                  />
+                </Link>
+                <div className={styles.body}>
+                  <span className={styles.userName}>{f.name}</span>
+                  <span className={styles.position}>{f.position}</span>
+                  <span className={styles.sendMessage}>Написать сообщение</span>
+                </div>
+              </div>
+            ))
+          ) : (
+            <h3>Пока друзей нет</h3>
+          )}
+        </div>
       </div>
-      <div className={styles.main}>
-        <Outlet />
-      </div>
-    </div>
+    </>
   );
 };
