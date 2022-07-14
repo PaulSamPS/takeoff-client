dfd3fdd16d54c2d82ef84baa6781501a301ecfb7
Pavel Samoylenko <psamoylenko2@gmail.com>
2022-07-15 00:45:13 +0300

    добавил эмоджи в модальное окно отправки сообщения

--- src/components/ModalMessage/ModalMessage.tsx
+++ src/components/ModalMessage/ModalMessage.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent } from 'react';
+import React, { ChangeEvent } from 'react';
 import styles from './ModalMessage.module.scss';
 import { Link } from 'react-router-dom';
 import { ModalMessageProps } from './ModalMessage.prop';
@@ -6,10 +6,23 @@ import { API_URL } from '../../http/axios';
 import { calculateTime } from '../../helpers/calculateTime';
 import { Button } from '../Button/Button';
 import { useChat } from '../../hooks/useChat';
+import { motion } from 'framer-motion';
+import { BaseEmoji, Picker } from 'emoji-mart';
+import { ReactComponent as SmileIcon } from '../../helpers/icons/smile.svg';
 
 export const ModalMessage = ({ friend, setModal, isModal }: ModalMessageProps) => {
-  const [text, setText] = React.useState<string | null>('');
+  const [text, setText] = React.useState<string>('');
+  const [showEmoji, setShowEmoji] = React.useState<boolean>(false);
   const { sendMessage } = useChat();
+
+  const addEmoji = (e: BaseEmoji) => {
+    setText(text + '' + e.colons);
+  };
+
+  const variantsModal = {
+    open: { opacity: 1, height: '20%' },
+    closed: { opacity: 0, height: 0 },
+  };
 
   const handleSendMessage = () => {
     localStorage.setItem('receiverUserId', friend.id!.toString());
@@ -42,23 +55,59 @@ export const ModalMessage = ({ friend, setModal, isModal }: ModalMessageProps) =
         </div>
       </div>
       <div className={styles.message}>
-        <div
-          className={styles.input}
-          id='message'
-          contentEditable='true'
+        <textarea
+          className={styles.textarea}
           placeholder='Написать сообщение...'
           role='textbox'
           aria-multiline='true'
-          onInput={(e: FormEvent<HTMLDivElement>) => setText(e.currentTarget.textContent)}
-        ></div>
-        <div className={styles.button}>
-          <Button
-            appearance='primary'
-            disabled={!(text && text.length > 0)}
-            onClick={handleSendMessage}
-          >
-            Отправить
-          </Button>
+          value={text}
+          onChange={(e: ChangeEvent<HTMLTextAreaElement>) => setText(e.target.value)}
+        />
+        <div className={styles.bottom}>
+          <div className={styles.picker}>
+            {showEmoji && (
+              <motion.div
+                className={styles.pickerModal}
+                animate={showEmoji ? 'open' : 'closed'}
+                variants={variantsModal}
+                initial={'closed'}
+                exit={'closed'}
+                transition={{
+                  duration: 0.5,
+                  type: 'spring',
+                }}
+              >
+                <Picker
+                  onSelect={addEmoji}
+                  skin={2}
+                  theme={'light'}
+                  perLine={8}
+                  set={'apple'}
+                  emojiSize={25}
+                  i18n={{ categories: { people: 'смайлы', recent: 'недавние' } }}
+                  style={{
+                    top: 'unset',
+                    right: 'unset',
+                    bottom: '77px',
+                    left: '-0',
+                    zIndex: '8',
+                  }}
+                />
+              </motion.div>
+            )}
+            <div>
+              <SmileIcon className={styles.emoji} onClick={() => setShowEmoji(!showEmoji)} />
+            </div>
+          </div>
+          <div className={styles.button}>
+            <Button
+              appearance='primary'
+              disabled={!(text && text.length > 0)}
+              onClick={handleSendMessage}
+            >
+              Отправить
+            </Button>
+          </div>
         </div>
       </div>
     </div>
