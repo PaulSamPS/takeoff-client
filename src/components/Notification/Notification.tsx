5248151beaae57a5b640dab8d38b5561452fcba8
Pavel Samoylenko <psamoylenko2@gmail.com>
2022-07-26 00:59:25 +0300

    модальное окно с оповещениями верстка,изменил хедер,сделал background при наведении и открытии окна в хедере

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -2,14 +2,43 @@ import React from 'react';
 import styles from './Notification.module.scss';
 import { useOnClickOutside } from '../../hooks/useOnclickOutside';
 import { NotificationProps } from './Notification.props';
+import { Link } from 'react-router-dom';
+import { API_URL } from '../../http/axios';
+import { useAppSelector } from '../../hooks/redux';
 
 export const Notification = ({ setVisibleNotification }: NotificationProps) => {
+  const loginUser = useAppSelector((state) => state.loginReducer.user);
   const ref = React.useRef<HTMLDivElement>(null);
   useOnClickOutside(ref, () => setVisibleNotification(false));
 
   return (
     <div className={styles.wrapper} ref={ref}>
-      Notification
+      <div className={styles.top}>
+        <span>Ваша страница</span>
+        <Link to={'#'}>Настройки</Link>
+      </div>
+      <div className={styles.notification}>
+        <img
+          src={loginUser.avatar == null ? `/photo.png` : `${API_URL}/avatar/${loginUser.avatar}`}
+          alt={loginUser.firstName + '' + loginUser.lastName}
+        />
+        <div className={styles.info}>
+          <span className={styles.infoText}>
+            <Link to={'#'} className={styles.user}>
+              {loginUser.firstName + ' ' + loginUser.lastName}
+            </Link>
+            <span>оценил вашу запись от</span>
+            <Link to={'#'}>26 июля в 20:20</Link>
+          </span>
+          <span>сегодня в 20:00</span>
+        </div>
+        <div className={styles.postImage}>
+          <img
+            src={loginUser.avatar == null ? `/photo.png` : `${API_URL}/avatar/${loginUser.avatar}`}
+            alt={loginUser.firstName + '' + loginUser.lastName}
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/layout/Main/Header/Header.tsx
+++ src/layout/Main/Header/Header.tsx
@@ -5,9 +5,12 @@ import { Link } from 'react-router-dom';
 import { API_URL } from '../../../http/axios';
 import { ReactComponent as ArrowDownIcon } from '../../../helpers/icons/arrowDown.svg';
 import { ReactComponent as NotificationIcon } from '../../../helpers/icons/notification.svg';
+import { ReactComponent as SearchIcon } from '../../../helpers/icons/search.svg';
 import { ProfileMenu } from '../../../components/ProfileMenu/ProfileMenu';
 import { Count } from '../../../components/Count/Count';
 import { Notification } from '../../../components/Notification/Notification';
+import { Input } from '../../../components/UI/Input/Input';
+import cn from 'classnames';
 
 export const Header = () => {
   const { user } = useAppSelector((state) => state.loginReducer);
@@ -20,12 +23,21 @@ export const Header = () => {
         <Link to={'/main/news'} className={styles.logo}>
           <h2>TakeOff</h2>
         </Link>
+        <div className={styles.search}>
+          <Input placeholder='Поиск' />
+          <SearchIcon />
+        </div>
         <div className={styles.notification} onClick={() => setVisibleNotification(true)}>
-          <NotificationIcon />
+          <div className={cn(styles.icon, { [styles.notificationVisible]: visibleNotification })}>
+            <NotificationIcon />
+          </div>
           <Count className={styles.count}>9</Count>
           {visibleNotification && <Notification setVisibleNotification={setVisibleNotification} />}
         </div>
-        <div className={styles.profile} onClick={() => setVisibleMenu(true)}>
+        <div
+          className={cn(styles.profile, { [styles.profileMenuVisible]: visibleMenu })}
+          onClick={() => setVisibleMenu(true)}
+        >
           <img
             src={user.avatar == null ? `/photo.png` : `${API_URL}/avatar/${user.avatar}`}
             alt={user.firstName + ' ' + user.lastName}
