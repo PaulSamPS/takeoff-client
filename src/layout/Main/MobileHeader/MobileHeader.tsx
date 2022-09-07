28787aa8c381d21518b25f1ae87bd836e8d8dc93
Pavel Samoylenko <psamoylenko2@gmail.com>
2022-09-07 21:50:36 +0300

    оповещения в мобильном хедере и различные правки под мобильную версиЮ

--- src/layout/Main/MobileHeader/MobileHeader.tsx
+++ src/layout/Main/MobileHeader/MobileHeader.tsx
@@ -3,15 +3,34 @@ import styles from './MobileHeader.module.scss';
 import { ReactComponent as BurgerIcon } from '../../../helpers/icons/burger.svg';
 import cn from 'classnames';
 import { MobileSidebar } from '../../../components/MobileSidebar/MobileSidebar';
+import { ReactComponent as NotificationIcon } from '../../../helpers/icons/notification.svg';
+import { useNotifications } from '../../../hooks/useNotifications';
+import { Count } from '../../../components/UI';
+import { NotificationList } from '../../../components/NotificationList/NotificationList';
 
 export const MobileHeader: React.FC<
   DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
 > = ({ className }) => {
   const [showSidebar, setShowSidebar] = React.useState<boolean>(false);
+  const [visibleNotification, setVisibleNotification] = React.useState<boolean>(false);
+
+  const { notificationsCount, handleReadNotifications } = useNotifications();
+
+  const handleOpenNotifications = () => {
+    setVisibleNotification(true);
+    handleReadNotifications();
+  };
 
   return (
     <div className={cn(styles.wrapper, className)}>
       <BurgerIcon onClick={() => setShowSidebar(true)} />
+      <div className={styles.notifications}>
+        <NotificationIcon onClick={handleOpenNotifications} />
+        {notificationsCount > 0 && <Count className={styles.count}>{notificationsCount}</Count>}
+        {visibleNotification && (
+          <NotificationList setVisibleNotification={setVisibleNotification} />
+        )}
+      </div>
       <MobileSidebar setModal={setShowSidebar} modal={showSidebar} />
     </div>
   );
