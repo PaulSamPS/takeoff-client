2c55f3adcab6054181f2122bd0fb2e61c4d60f82
Pavel Samoylenko <psamoylenko2@gmail.com>
2022-09-18 07:27:57 +0300

    .

--- src/helpers/socketContext.ts
+++ src/helpers/socketContext.ts
@@ -5,7 +5,7 @@ export const SocketContext = React.createContext<Socket | null>(null);
 export const socketContext = () => {
   const [socket, setSocket] = React.useState<Socket | null>(null);
 
-  const URL = 'https://176.113.83.209:4000';
+  const URL = 'https://paulsam.ru';
   React.useEffect(() => {
     const newSocket = io(URL, { transports: ['websocket'] });
     newSocket.connect();
